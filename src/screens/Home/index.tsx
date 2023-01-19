622b79fcc40fac12100c79714606ea4b45c4058e
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-01-19 10:17:52 -0300

    :lipstick: add Heading component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,7 @@
 import { View, Image } from 'react-native';
 
 import logoImg from '../../assets/logo-nlw-esports.png';
+import { Heading } from '../../components/Heading';
 
 import { styles } from './styles';
 
@@ -8,6 +9,11 @@ export function Home() {
   return (
     <View style={styles.container}>
       <Image source={logoImg} style={styles.logo} />
+
+      <Heading
+        title="Encontre seu duo!"
+        subtitle="Selecione o game que deseja jogar..."
+      />
     </View>
   );
 }
